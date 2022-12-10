382f1b2641a0c87e00baa71939ebba3ec16c7f2f
Nousheen Begum <111900257+Nousheenb@users.noreply.github.com>
2022-12-10 21:33:47 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,9 @@
+#band name generator
+
+''' learned about basic things in python , like how i can take input and display output '''
+
+place = input("What's your native country ?")
+
+pet = input("What's your pet name ?")
+
+print("Your band bame is "+ place + pet )
